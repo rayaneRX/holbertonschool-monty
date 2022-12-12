b66070f6650059fb955ab24a8c6d690bd73b9011
rChrisb <5767@holbertonstudents.com>
2022-12-12 11:52:06 +0100

    Header file

--- main.h
+++ main.h
@@ -3,6 +3,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
