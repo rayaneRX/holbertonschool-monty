9d2940bd913ef6efebb4e2fcbc4af510984f8b62
rChrisb <5767@holbertonstudents.com>
2022-12-13 14:55:19 +0100

    Monty byte code file

---
+++ pint.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+
+/**
+ * monty_pint - prints the value at the top of the stack
+ * @stack : is a stack
+ * @line_number : the number of the line
+ * Return: nothing
+ */
+void monty_pint(stack_t **stack, unsigned int line_number)
+{
+	if (!(*stack))
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	else
+		printf("%d\n", (*stack)->n);
+}
