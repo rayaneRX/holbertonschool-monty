9532df290ae1e90828d8cb8fb6a249b511309ac3
rayaneRX <elkerayane@gmail.com>
2022-12-13 23:34:31 +0100

    add the pop

---
+++ pop.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+/**
+ * monty_pop - function removes the top element of the stack
+ * @stack : stack
+ * @line_number : the number of the line
+ * Return: nothing
+ */
+void monty_pop(stack_t **stack, unsigned int line_number)
+{
+	if (!(*stack))
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	
+    stack_t *tmp = *stack;
+
+    if (!tmp)
+		return;
+	
+    
+    if (tmp->next)
+	{
+		tmp->next->prev = tmp->prev;
+	}
+	*stack = tmp->next;
+	free(tmp);
+}
